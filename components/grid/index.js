0c747efb4c9b571f8cb609ffa3693204f5f13126
caijf <369756941@qq.com>
2018-06-04 23:30:04 +0800

    grid组件

--- components/grid/index.js
+++ components/grid/index.js
@@ -3,24 +3,78 @@ import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 
+const justifyMap = {
+    start: 'flex-start',
+    end: 'flex-end'
+}
+
+const alignMap = {
+    top: 'flex-start',
+    bottom: 'flex-end'
+}
+
 const StyledRow = styled.div `
     display: flex;
     flex-flow: row wrap;
     box-sizing: border-box;
     width: 100%;
+
+    ${props=>{
+        let _st = '';
+
+        if(props.align){
+            _st += 'align-items:' + (alignMap[props.align] || props.align) + ';';
+        }
+
+        if(props.justify){
+            _st += 'justify-content:' + (justifyMap[props.justify] || props.justify) + ';';
+        }
+
+        return _st;
+
+    }}
 `;
 
 const StyledCol = styled.div `
     box-sizing: border-box;
-    flex: 1;
-    margin-left: ${props=>props.gutter || 0}px;
-    
-    ${props=>(typeof props.order === 'number') ? 'order:' + props.order + ';' : ''}
-    ${props=>(typeof props.span === 'number') ? 'flex:' + props.span + ';' : ''}
 
-    &:first-child{
-        margin-left: 0;
-    }
+    ${props=>{
+        let _st = '';
+
+        if(props.span){
+            if(typeof props.span === 'number'){
+                _st += 'width:' + props.span + 'px;';
+            }else if(props.span.indexOf('px') > -1 || props.span.indexOf('%') > -1){
+                _st += 'width:' + props.span + ';';
+            // }else if(props.span.indexOf('%') > -1){
+            //     _st += 'flex:0 1 ' + props.span + ';';
+            }else{
+                _st += 'flex: 1;';
+            }
+        }else{
+            _st += 'flex: 1;';
+        }
+
+        if(props.order){
+            _st += 'order:' + props.order + ';';
+        }
+
+        if(props.align){
+            _st += 'align-self:' + (alignMap[props.align] || props.align) + ';';
+        }
+
+        if(props.gutter){
+            if(typeof props.gutter === 'number'){
+                _st += 'margin-left: ' + props.gutter + 'px;';
+                _st += '&:first-child{ margin-left: 0; }';
+            }else if(typeof props.gutter === 'string' && (props.gutter.indexOf('px') > -1 || props.gutter.indexOf('%') > -1)){
+                _st += 'margin-left: ' + props.gutter + ';';
+                _st += '&:first-child{ margin-left: 0; }';
+            }
+        }
+
+        return _st;
+    }}
 `;
 
 const GutterContext = React.createContext(0);
@@ -29,7 +83,11 @@ const {Provider, Consumer} = GutterContext;
 export class Col extends React.Component{
     static propTypes = {
         order: PropTypes.number,
-        span: PropTypes.number
+        span: PropTypes.oneOfType([
+            PropTypes.number,
+            PropTypes.string
+        ]),
+        align: PropTypes.oneOf(['top', 'center', 'bottom'])
     }
 
     render(){
@@ -49,7 +107,7 @@ export class Row extends React.Component{
     }
 
     render(){
-        const {gutter, justify, align, ...rest} = this.props;
+        const {gutter, ...rest} = this.props;
 
         return(
             <Provider value={gutter || 0}>
--- examples/app.js
+++ examples/app.js
@@ -6,25 +6,94 @@ import styled, {injectGlobal} from 'styled-components'
 import {Row, Col} from '../components/grid'
 
 injectGlobal`
-    
-`;
+    blockquote{
+        font-size: 12px;
+        color: #999;
+    }
 
-const StyledGridDemo = styled.div`
-    .row{
-        margin-bottom: 10px;
+    table{
+        border-collapse: collapse;
+        border-spacing: 0;
+        empty-cells: show;
+        border: 1px solid #ebedf0;
+        width: 100%;
+        margin: 8px 0 16px;
+        font-size: 14px;
+        line-height: 1.5;
+        color: #314659;
 
-        &:last-child{
-            margin-bottom: 0;
+        th, td{
+            border: 1px solid #ebedf0;
+            padding: 16px 24px;
+            text-align: left;
         }
 
-        div{
-            border: 1px solid #000;
-            background: #fff;
-            color: #000;
-            text-align: center;
+        th{
+            white-space: nowrap;
+            color: #5c6b77;
+            font-weight: 500;
+            background: rgba(0,0,0,.02);
+        }
+
+        td{
+            font-weight: 500;
+            width: 20%;
+            font-family: Lucida Console,Consolas,Menlo,Courier,monospace;
         }
     }
 `;
+
+const StyledGridDemo = styled.div`
+    .row-demo{
+        padding-top: 10px;
+
+        >div{
+            text-align: center;
+            color: #fff;
+            background: #00a0e9;
+        }
+        >div:nth-child(2n){
+            background: rgba(0,160,233,.7);
+        }
+
+        &:last-child{
+            padding-bottom: 10px;
+        }
+    }
+    .row-demo-1{
+        background-image: linear-gradient(90deg,#f5f5f5 4.16666667%,transparent 0,transparent 8.33333333%,#f5f5f5 0,#f5f5f5 12.5%,transparent 0,transparent 16.66666667%,#f5f5f5 0,#f5f5f5 20.83333333%,transparent 0,transparent 25%,#f5f5f5 0,#f5f5f5 29.16666667%,transparent 0,transparent 33.33333333%,#f5f5f5 0,#f5f5f5 37.5%,transparent 0,transparent 41.66666667%,#f5f5f5 0,#f5f5f5 45.83333333%,transparent 0,transparent 50%,#f5f5f5 0,#f5f5f5 54.16666667%,transparent 0,transparent 58.33333333%,#f5f5f5 0,#f5f5f5 62.5%,transparent 0,transparent 66.66666667%,#f5f5f5 0,#f5f5f5 70.83333333%,transparent 0,transparent 75%,#f5f5f5 0,#f5f5f5 79.16666667%,transparent 0,transparent 83.33333333%,#f5f5f5 0,#f5f5f5 87.5%,transparent 0,transparent 91.66666667%,#f5f5f5 0,#f5f5f5 95.83333333%,transparent 0);
+
+        >div{
+            background: rgba(0,160,233,.5);
+        }
+        >div:nth-child(2n){
+            background: hsla(0,0%,100%,.2);
+            color: #999;
+        }
+    }
+
+    .row-demo-2{
+        background: #f5f5f5;
+    }
+`;
+
+const StyledPlaceHolder = styled.div `
+    ${props=>{
+        if(props.height > 20){
+            return 'height: ' + props.height + 'px; line-height:' + props.height + 'px;';
+        }else{
+            return 'height: 50px; line-height: 50px;' 
+        }
+    }}
+`;
+
+class PlaceHolder extends React.Component{
+    render(){
+        return (
+            <StyledPlaceHolder {...this.props} />
+        )
+    }
+}
 
 
 class App extends React.Component {
@@ -32,47 +101,292 @@ class App extends React.Component {
         return (
             <div>
                 <div>a simple app examples preview.</div>
+                <h2>Grid</h2>
+                <h3>基础删格</h3>
+                <blockquote>Col组件支持 span 设置宽度，仅支持百分比和px。</blockquote>
                 <StyledGridDemo>
-                    <h3>Grid</h3>
-                    <Row className="row">
-                        <Col>12</Col>
+                    <Row className="row-demo row-demo-1" gutter={10}>
+                        <Col>
+                            <PlaceHolder>100%</PlaceHolder>
+                        </Col>
                     </Row>
-                    <Row className="row">
-                        <Col>6</Col>
-                        <Col>6</Col>
+                    <Row className="row-demo row-demo-1">
+                        <Col>
+                            <PlaceHolder>50%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>50%</PlaceHolder>
+                        </Col>
                     </Row>
-                    <Row className="row">
-                        <Col>4</Col>
-                        <Col>4</Col>
-                        <Col>4</Col>
+                    <Row className="row-demo row-demo-1">
+                        <Col span="66.66%">
+                            <PlaceHolder>66.66%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>33.33%</PlaceHolder>
+                        </Col>
                     </Row>
-                    <Row gutter={10} className="row">
-                        <Col>3</Col>
-                        <Col>3</Col>
-                        <Col>3</Col>
-                        <Col>3</Col>
+                    <Row className="row-demo row-demo-1">
+                        <Col>
+                            <PlaceHolder>33.33%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>33.33%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>33.33%</PlaceHolder>
+                        </Col>
                     </Row>
-                    <Row className="row">
-                        <Col>2</Col>
-                        <Col>2</Col>
-                        <Col>2</Col>
-                        <Col>2</Col>
-                        <Col>2</Col>
-                        <Col>2</Col>
+                    <Row className="row-demo row-demo-1">
+                        <Col>
+                            <PlaceHolder>25%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>25%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>25%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>25%</PlaceHolder>
+                        </Col>
                     </Row>
-                    <Row gutter={5} className="row">
-                        <Col>1</Col>
-                        <Col>1</Col>
-                        <Col gutter={20} order={2}>10</Col>
-                        <Col span={3}>1</Col>
-                        <Col>1</Col>
-                        <Col>1</Col>
-                        <Col>1</Col>
-                        <Col>1</Col>
-                        <Col>1</Col>
-                        <Col>1</Col>
+                    <Row className="row-demo row-demo-1">
+                        <Col>
+                            <PlaceHolder>20%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>20%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>20%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>20%</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>20%</PlaceHolder>
+                        </Col>
                     </Row>
                 </StyledGridDemo>
+                <h3>区间间隔</h3>
+                <blockquote>Row组件的gutter属性是对全部Col组件进行设置</blockquote>
+                <blockquote>单独Col也可进行设置gutter，如果Row也有该值会覆盖Row的gutter</blockquote>
+                <StyledGridDemo>
+                    <Row className="row-demo row-demo-2" gutter={10}>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2">
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col gutter="33.33%">
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                </StyledGridDemo>
+                <h3>水平对齐</h3>
+                <blockquote>Row组件的 justify 支持 start、center、end、space-between、space-around</blockquote>
+                <StyledGridDemo>
+                    <Row className="row-demo row-demo-2" justify="start">
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" justify="center">
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" justify="end">
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" justify="space-between">
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" justify="space-around">
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                        <Col span={"20%"}>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                </StyledGridDemo>
+                <h3>垂直对齐</h3>
+                <blockquote>Row组件的 align 支持 top、center、bottom</blockquote>
+                <StyledGridDemo>
+                    <Row className="row-demo row-demo-2" align="top">
+                        <Col>
+                            <PlaceHolder height={100}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={70}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={30}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" align="center">
+                        <Col>
+                            <PlaceHolder height={100}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={70}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={30}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                    <Row className="row-demo row-demo-2" align="bottom">
+                        <Col>
+                            <PlaceHolder height={100}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={70}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder height={30}>col</PlaceHolder>
+                        </Col>
+                        <Col>
+                            <PlaceHolder>col</PlaceHolder>
+                        </Col>
+                    </Row>
+                </StyledGridDemo>
+                <h3>删格排序</h3>
+                <blockquote>Col组件的order值越小越靠前</blockquote>
+                <StyledGridDemo>
+                    <Row className="row-demo row-demo-2">
+                        <Col order={4}>
+                            <PlaceHolder>order-4 col-1</PlaceHolder>
+                        </Col>
+                        <Col order={3}>
+                            <PlaceHolder>order-3 col-2</PlaceHolder>
+                        </Col>
+                        <Col order={2}>
+                            <PlaceHolder>order-2 col-3</PlaceHolder>
+                        </Col>
+                        <Col order={1}>
+                            <PlaceHolder>order-1 col-4</PlaceHolder>
+                        </Col>
+                    </Row>
+                </StyledGridDemo>
+                <h3>API</h3>
+                <h4>Row</h4>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>属性</th>
+                            <th>说明</th>
+                            <th>类型</th>
+                            <th>默认值</th>
+                        </tr>
+                        <tr>
+                            <td>gutter</td>
+                            <td>栅格间隔。数字、px值、百分比</td>
+                            <td>number/string</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>justify</td>
+                            <td>水平排列方式。start end center space-around space-between</td>
+                            <td>string</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>align</td>
+                            <td>垂直对齐方式。top middle bottom</td>
+                            <td></td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h4>Col</h4>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>属性</th>
+                            <th>说明</th>
+                            <th>类型</th>
+                            <th>默认值</th>
+                        </tr>
+                        <tr>
+                            <td>gutter</td>
+                            <td>栅格间隔，可以覆盖Row组件也设置gutter。数字、px值、百分比</td>
+                            <td>number/string</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>span</td>
+                            <td>删格宽度，设置宽度后去掉flex:1。数字、px值、百分比</td>
+                            <td>number/string</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>align</td>
+                            <td>垂直对齐方式。top middle bottom</td>
+                            <td>string</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>order</td>
+                            <td>栅格顺序，值越小越靠前</td>
+                            <td>number</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
