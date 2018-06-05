986e1e5d6a421247cc7120c2f4ed90f488c19c13
蔡金锋 <jeff.cai@99bill.com>
2018-06-05 17:50:26 +0800

    按钮组件

--- components/button/index.js
+++ components/button/index.js
@@ -1,22 +1,143 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
-const StyledRow = styled.div `
+import createTag from '../utils/createTag'
+
+// TODO ThemeProvider
+
+const ButtonTag = createTag({
+    tag: 'button',
+    propsToOmit: ['fill', 'round', 'active', 'size', 'inline', 'color', 'disabled']
+});
+
+const isWhiteColor = (color)=>{
+    return color === 'white' || color === '#fff' || color === '#ffffff';
+}
+
+const blackColor = '#000',
+    whiteColor = '#fff';
+
+const disabledCss = css `
+    opacity: .4;
+
+    &:active{
+        opacity: .4;
+    }
+`;
+
+const StyledButton = styled(ButtonTag) `
+    position: relative;
+    outline: 0 none;
+    box-sizing: border-box;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-word;
+    white-space: nowrap;
+    border: 0 none;
+    padding: 0 15px;
+
+    ${props=>{
+        // 处理按钮内联、尺寸、不填充、圆角、颜色等样式
+        let bgColor = props.color,
+            color = whiteColor,
+            height = '44px',
+            fontSize = '18px',
+            width = '100%',
+            display = 'block',
+            round = ((typeof props.round === 'number' && props.round >=0) ? props.round : 5) + 'px';
+
+        if(props.inline){
+            display = 'inline-block';
+            width = 'auto';
+        }
+
+        if(props.size && props.size === 'small'){
+            fontSize = '14px';
+            height = '30px';
+        }
+
+        if(props.fill){
+            if(isWhiteColor(props.color)){
+                bgColor = whiteColor;
+                color = blackColor;
+            }
+        }else{
+            bgColor = 'transparent';
+            color = props.color;
+        }
+
+        return [
+            'background:' + bgColor,
+            'color:' + color,
+            'height:' + height,
+            'line-height:' + height,
+            'font-size:' + fontSize,
+            'width:' + width,
+            'display:' + display,
+            'border-radius:' + round
+        ].join(';')
+    }}
+
+    &:active{
+        opacity: 0.7;
+    }
+
+    &::after{
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 200%;
+        height: 200%;
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        transform-origin: 0 0;
+        transform: scale(.5);
+        box-sizing: border-box;
+        pointer-events: none;
+    }
+
+    ${props => {
+        // 处理边框颜色和圆角
+        let _afterStyle = '';
+        let round = (((typeof props.round === 'number' && props.round >=0) ? props.round : 5)*2) + 'px';
+
+        _afterStyle = '&::after{ border-color:' + props.color + '; border-radius: ' + round + ';}';
+
+        return _afterStyle;
+    }}
+
+    ${props=>props.disabled ? disabledCss : ''}
 
 `;
 
 export default class Button extends React.Component{
-    // static propTypes = {
-    //     gutter: PropTypes.number,
-    //     justify: PropTypes.oneOf(['start', 'center', 'end', 'space-between', 'space-around']),
-    //     align: PropTypes.oneOf(['top', 'center', 'bottom'])
-    // }
+    static propTypes = {
+        size: PropTypes.oneOf(['small', 'large']),
+        color: PropTypes.string,
+        round: PropTypes.number,
+        fill: PropTypes.bool,
+        active: PropTypes.bool,
+        inline: PropTypes.bool,
+        disabled: PropTypes.bool,
+        as: PropTypes.string
+    }
+
+    static defaultProps = {
+        size: 'large',
+        color: 'white',
+        round: 5,
+        active: false,
+        fill: true,
+        inline: false
+    }
 
     render(){
         return(
-            <a href="#">按钮</a>
+            <StyledButton {...this.props} />
         )
     }
 }
--- examples/button-example.js
+++ examples/button-example.js
@@ -5,6 +5,9 @@ import styled, {injectGlobal} from 'styled-components'
 import Button from '../components/button'
 
 injectGlobal`
+    body{
+        background: #f5f5f5;
+    }
     blockquote{
         font-size: 12px;
         color: #999;
@@ -20,6 +23,7 @@ injectGlobal`
         font-size: 14px;
         line-height: 1.5;
         color: #314659;
+        background: #fff;
 
         th, td{
             border: 1px solid #ebedf0;
@@ -42,14 +46,107 @@ injectGlobal`
     }
 `;
 
+const StyledDemo = styled.div`
+    button{
+        margin-bottom: 5px;
+    }
+`;
+
+const colors = {
+    blue: '#007aff',
+    red: '#ff3b30',
+    yellow: '#fc0',
+    black: '#000'
+}
 
 export default class Example extends React.Component {
     render(){
         return (
             <div>
                 <h2>Button</h2>
-                <h3>基础</h3>
-                <Button>按钮</Button>
+                <StyledDemo>
+                    <h3>基础</h3>
+                    <Button>默认按钮</Button>
+                    <h3>尺寸</h3>
+                    <blockquote>size</blockquote>
+                    <Button size="small">small按钮</Button>
+                    <Button>default按钮</Button>
+                    <h3>颜色</h3>
+                    <blockquote>color</blockquote>
+                    <Button color={colors.blue}>按钮</Button>
+                    <Button color={colors.red}>按钮</Button>
+                    <Button size="small" color={colors.yellow}>按钮</Button>
+                    <Button size="small" color={colors.black}>按钮</Button>
+                    <h3>不填充</h3>
+                    <blockquote>fill</blockquote>
+                    <Button fill={false} color={colors.blue}>按钮</Button>
+                    <Button size="small" fill={false} color={colors.red}>按钮</Button>
+                    <h3>内联</h3>
+                    <blockquote>inline</blockquote>
+                    <Button inline fill={false} color={colors.blue}>按钮</Button>
+                    <Button inline color={colors.red}>按钮</Button>
+                    <Button inline fill={false} size="small" color={colors.yellow}>按钮</Button>
+                    <Button inline size="small" color={colors.black}>按钮</Button>
+                    <h3>圆角</h3>
+                    <blockquote>round</blockquote>
+                    <Button round={0} color={colors.blue}>按钮22</Button>
+                    <Button round={20} color={colors.red}>按钮22</Button>
+                    <h3>禁用</h3>
+                    <blockquote>disabled</blockquote>
+                    <Button disabled fill={false} color={colors.blue}>按钮</Button>
+                    <Button disabled color={colors.red}>按钮</Button>
+                    <Button disabled fill={false} inline size="small" color={colors.blue}>按钮</Button>
+                    <Button disabled inline size="small" color={colors.red}>按钮</Button>
+
+                    <h3>API</h3>
+                    <h4>Button</h4>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>属性</th>
+                                <th>说明</th>
+                                <th>类型</th>
+                                <th>默认值</th>
+                            </tr>
+                            <tr>
+                                <td>size</td>
+                                <td>大小，仅支持 small</td>
+                                <td>string</td>
+                                <td>large</td>
+                            </tr>
+                            <tr>
+                                <td>color</td>
+                                <td>颜色</td>
+                                <td>string</td>
+                                <td>white</td>
+                            </tr>
+                            <tr>
+                                <td>fill</td>
+                                <td>填充</td>
+                                <td>boolean</td>
+                                <td>true</td>
+                            </tr>
+                            <tr>
+                                <td>inline</td>
+                                <td>内联</td>
+                                <td>boolean</td>
+                                <td>false</td>
+                            </tr>
+                            <tr>
+                                <td>round</td>
+                                <td>圆角</td>
+                                <td>number</td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>disabled</td>
+                                <td>禁用</td>
+                                <td>boolean</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </StyledDemo>
             </div>
         )
     }
